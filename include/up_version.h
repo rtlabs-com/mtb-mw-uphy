3c3a5c10d346e26623d152cca60c79de297ac3e4
Mattias Ågren <mattias.agren@rt-labs.com>
2025-04-07 15:59:30 +0200

    Rebuilt lib using modus toolbox 3.4
    * Also enabled stripping library
    
    Change-Id: I07442cc50c4e986534467f282dd50c18f7048875

--- include/up_version.h
+++ include/up_version.h
@@ -13,7 +13,7 @@
 #ifndef UP_VERSION_H
 #define UP_VERSION_H
 
-#define UPHY_GIT_REVISION "v1.0.1-4-g4228e97"
+#define UPHY_GIT_REVISION "v1.0.1-5-gce073a4"
 
 #if !defined(UP_VERSION_BUILD) && defined(UPHY_GIT_REVISION)
 #define UP_VERSION_BUILD UPHY_GIT_REVISION
