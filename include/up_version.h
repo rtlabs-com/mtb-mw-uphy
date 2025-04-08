517d7bccf4f6f1e357ed876d85929ca9831a89b3
Mattias Ågren <mattias.agren@rt-labs.com>
2025-04-08 10:25:15 +0200

    Release v0.7.0
    * Built from mtb-up-core 74184658
    
    Change-Id: I74edf70e3bb0deea570541425ae5e688feab63fc

--- include/up_version.h
+++ include/up_version.h
@@ -13,7 +13,7 @@
 #ifndef UP_VERSION_H
 #define UP_VERSION_H
 
-#define UPHY_GIT_REVISION "v1.0.1-5-gce073a4"
+#define UPHY_GIT_REVISION "release-v0.7.0"
 
 #if !defined(UP_VERSION_BUILD) && defined(UPHY_GIT_REVISION)
 #define UP_VERSION_BUILD UPHY_GIT_REVISION
